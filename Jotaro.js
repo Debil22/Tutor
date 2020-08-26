9be0b17fcae79a5a56e81d32aac03d6950e3fd96
Debil22 <ilyaboya18@gmail.com>
2020-08-26 10:04:05 +0300

    JOTARO

--- Jotaro.js
+++ Jotaro.js
@@ -1,13 +1,13 @@
 // Задаем функцию для проверки возраста
 function checkAge() {
- if (age > 18) {
+ if (age > 17) {
  	return true;
  } else {
  	return confirm('HOHO')
  }
 }
 
-let age = prompt('How old are u?', 18);
+let age = prompt('How old are u?');
 
 if (checkAge(age)) {
 	alert('Jotaro')
